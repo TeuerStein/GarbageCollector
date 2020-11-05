74682ea10fff5fb53b2f7b20d06e5d87afc25340
TeuerStein <xenkai123456789@gmail.com>
2020-11-06 00:19:30 +0100

    Was added tests. Fixed bugs with names of types/functions/variables

--- GarbageCollector.c
+++ GarbageCollector.c
@@ -7,10 +7,10 @@
 typedef enum {
     INT,
     TWIN
-}oType;
+}ObjectType;
 
 typedef struct sObject {
-    oType type;
+    ObjectType type;
     unsigned char marked;
     
     struct sObject* next;
@@ -20,7 +20,7 @@ typedef struct sObject {
         
         struct {
             struct sObject* head;
-            struct sObject* title;
+            struct sObject* tail;
         };
     };
 }Object;
@@ -32,53 +32,25 @@ typedef struct {
     Object* firstObject;
     
     int numObjects;
-}vm;
+    int maxObjects;
+}VM;
 
-void push(vm* vm, Object* value) {
+void push(VM* vm, Object* value) {
     vm->stack[vm->stackSize++] = value;
 }
 
-Object* pop(vm* vm) {
+Object* pop(VM* vm) {
     return vm->stack[--vm->stackSize];
 }
 
-vm* newVm() {
-    vm* mainVm = (vm*)malloc(sizeof(vm));
-    mainVm->stackSize = 0;
-    mainVm->firstObject = NULL;
-    mainVm->numObjects = 0;
-    mainVm->maxObjects = IGCT;
+VM* newVM() {
+    VM* vm = (VM*)malloc(sizeof(VM));
+    vm->stackSize = 0;
+    vm->firstObject = NULL;
+    vm->numObjects = 0;
+    vm->maxObjects = IGCT;
     
-    return mainVm;
-}
-
-Object* newObject(vm* vm, oType type) {
-    if(vm->numObjects == vm->maxObjects) gc(vm);
-    Object* object = (Object*) malloc(sizeof(Object))
-    object->type = type;
-    
-    vm->maxObjects++;
-    return object;
-}
-
-void pushInt(vm* vm, int intV) {
-    Object* object = newObject(vm, INT);
-    object->value = intV;
-}
-
-Object* pushTwin(vm* vm) {
-    Object* object = newObject(vm, TWIN);
-    object->tail = pop(vm);
-    object->head = pop(vm);
-    
-    push(vm, object);
-    return object;
-}
-
-void markAll(vm* vm) {
-    for(int i = 0; i < vm->stack; i++) {
-        mark(vm->stack[i]);
-    }
+    return vm;
 }
 
 void mark(Object* object) {
@@ -92,14 +64,21 @@ void mark(Object* object) {
     }
 }
 
-void marksweep(vm* vm) {
+void markAll(VM* vm) {
+    for(int i = 0; i < vm->stackSize; i++) {
+        mark(vm->stack[i]);
+    }
+}
+
+void marksweep(VM* vm) {
     Object** object = &vm->firstObject;
     
     while(*object) {
         if (!(*object)->marked) {
             Object* unreached = *object;
             *object = unreached->next;
-            free(unreached)
+            
+            free(unreached);
             
             vm->numObjects--;
         } else {
@@ -109,7 +88,7 @@ void marksweep(vm* vm) {
     }
 }
 
-void gc(vm* vm) {
+void gc(VM* vm) {
     int numObjects = vm->numObjects;
     
     markAll(vm);
@@ -117,9 +96,38 @@ void gc(vm* vm) {
     
     vm->maxObjects = vm->numObjects * 2;
     
-    printf("Was deleted %d objects, %d objects left", numObjects - vm->numObjects, vm->numObjects);
+    printf("Was deleted %d objects, %d objects left.\n", numObjects - vm->numObjects, vm->numObjects);
 }
-void printObj(Object* object) {
+
+Object* newObject(VM* vm, ObjectType type) {
+    if(vm->numObjects == vm->maxObjects) gc(vm);
+    Object* object = (Object*)malloc(sizeof(Object));
+    object->type = type;
+    object->next = vm->firstObject;
+    vm->firstObject = object;
+    object->marked = 0;
+    
+    vm->numObjects++;
+    return object;
+}
+
+void pushInt(VM* vm, int intValue) {
+    Object* object = newObject(vm, INT);
+    object->value = intValue;
+    
+    push(vm, object);
+}
+
+Object* pushPair(VM* vm) {
+    Object* object = newObject(vm, TWIN);
+    object->tail = pop(vm);
+    object->head = pop(vm);
+    
+    push(vm, object);
+    return object;
+}
+
+void objectPrint(Object* object) {
     switch(object->type) {
         case INT:
             printf("%d", object->value);
@@ -127,12 +135,73 @@ void printObj(Object* object) {
         
         case TWIN:
             printf("(");
-            printObj(object->head);
+            objectPrint(object->head);
             printf(", ");
-            printObj(object->tail);
+            objectPrint(object->tail);
             printf(")");
             break;
     }
+}
+
+void freeVM(VM* vm) {
+    vm->stackSize = 0;
+    
+    gc(vm);
+    free(vm);
+}
+
+void first_test() {
+    printf("1: Objects on stack was saved.\n");
+    VM* vm = newVM();
+    pushInt(vm, 1);
+    pushInt(vm, 2);
+    
+    gc(vm);
+    freeVM(vm);
+}
+
+void second_test() {
+    printf("2: Unreached objects are collected.\n");
+    VM* vm = newVM();
+    pushInt(vm, 1);
+    pushInt(vm, 2);
+    pop(vm);
+    pop(vm);
+    
+    gc(vm);
+    freeVM(vm);
+}
+
+void third_test() {
+    printf("3: Reach the nested objects.\n");
+    VM* vm = newVM();
+    pushInt(vm, 1);
+    pushInt(vm, 2);
+    pushPair(vm);
+    pushInt(vm, 3);
+    pushInt(vm, 4);
+    pushPair(vm);
+    pushPair(vm);
+    
+    gc(vm);
+    freeVM(vm);
+}
+
+void another_test() {
+    printf("4: Cycles.\n");
+    VM* vm = newVM();
+    pushInt(vm, 1);
+    pushInt(vm, 2);
+    Object* a = pushPair(vm);
+    pushInt(vm, 3);
+    pushInt(vm, 4);
+    Object* b = pushPair(vm);
+
+    a->tail = b;
+    b->tail = a;
+
+    gc(vm);
+    freeVM(vm);
 }
 
 int main(int artc, const char** argv) {
