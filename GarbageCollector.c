fb7a1487b8c762cf7b76deca4404b9441e290009
TeuerStein <xenkai123456789@gmail.com>
2020-11-05 16:56:22 +0100

    Was added enum and sObject structure for identified type of object

--- GarbageCollector.c
+++ GarbageCollector.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    INT,
+    TWIN
+}oType;
+
+typedef struct sObject {
+    oType type;
+    
+    union {
+        int value;
+        
+        struct {
+            struct sObject* head;
+            struct sObject* title;
+        };
+    };
+};
+
 int main(int artc, const char** argv) {
     
     return 0;
