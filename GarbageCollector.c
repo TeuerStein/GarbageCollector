31fb73b3acb51e1dfd3926607eab344714eb9d6f
TeuerStein <xenkai123456789@gmail.com>
2020-11-05 17:23:52 +0100

    Added structure for stack and 2 operation ( push & pop ). Initialized Virtual Machine. Added function for create the objects

--- GarbageCollector.c
+++ GarbageCollector.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STACK_MAX_SIZE 256
+
 typedef enum {
     INT,
     TWIN
@@ -17,7 +19,33 @@ typedef struct sObject {
             struct sObject* title;
         };
     };
-};
+}Object;
+
+typedef struct {
+    Object* stack[STACK_MAX_SIZE];
+    
+    int stackSize;
+}vm;
+
+void push(vm* vm, Object* value) {
+    vm->stack[vm->stackSize++] = value;
+}
+
+Object* pop(vm* vm) {
+    return vm->stack[--vm->stackSize];
+}
+
+vm* newVm() {
+    vm* mainVm = (vm*)malloc(sizeof(vm));
+    mainVm->stackSize = 0;
+    return mainVm;
+}
+
+Object* newObject(vm* vm, oType type) {
+    Object* object = (Object*) malloc(sizeof(Object))
+    object->type = type;
+    return object;
+}
 
 int main(int artc, const char** argv) {
     
